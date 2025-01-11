dce57141747c63f13f596402d35545ff21f8f00d
Thinh <thinhptqph47145@fpt.edu.vn>
2025-01-11 10:41:07 +0700

    create/apiUser

--- be/app.js
+++ be/app.js
@@ -3,13 +3,19 @@ import mongoose from "mongoose";
 import cors from "cors";
 import morgan from "morgan";
 import productRouter from "./routers/products/products";
+import userRouter from "./routers/users/user.js";
 const app = express();
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
 
 // routers
-app.use(`/api`, productRouter);
+app.use(`/api`, userRouter);
 // Kết nối tới MongoDB
 mongoose.connect("mongodb://localhost:27017/DATN");
 
