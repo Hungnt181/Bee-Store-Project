8dab70c38a25659af4b3be966837666e94e39567
dangtien0104 <dangtien0104@gmail.com>
2025-01-11 10:49:53 +0700

    basic api color

--- be/app.js
+++ be/app.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import morgan from "morgan";
 import productRouter from "./routers/products/products";
+import { colorRouter } from "./routers/colors/color";
 const app = express();
 app.use(cors());
 app.use(morgan("dev"));
@@ -10,6 +11,7 @@ app.use(express.json());
 
 // routers
 app.use(`/api`, productRouter);
+app.use(`/api`, colorRouter);
 // Kết nối tới MongoDB
 mongoose.connect("mongodb://localhost:27017/DATN");
 
