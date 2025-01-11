159f5be2066f8f5aab1dc576cd5fc8f2c6392ca7
quin288 <quynhmeo288@gmail.com>
2025-01-11 11:05:40 +0700

    basic api payment

--- be/app.js
+++ be/app.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import morgan from "morgan";
 import productRouter from "./routers/products/products";
+import { paymentRouter } from "./routers/payments/payment";
 const app = express();
 app.use(cors());
 app.use(morgan("dev"));
@@ -10,6 +11,7 @@ app.use(express.json());
 
 // routers
 app.use(`/api`, productRouter);
+app.use(`/api`, paymentRouter);
 // Kết nối tới MongoDB
 mongoose.connect("mongodb://localhost:27017/DATN");
 
