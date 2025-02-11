cc6b591aeab64b4e7a12317925e2a2b24de27179
hienvuong0307 <nguyenhienvuong219@gmail>
2025-02-11 10:45:00 +0700

    completed homepage

--- fe/src/pages/Home/_components/SlideShowBanner.tsx
+++ fe/src/pages/Home/_components/SlideShowBanner.tsx
@@ -1,10 +1,19 @@
 import { LeftOutlined, RightOutlined } from "@ant-design/icons";
 import { Carousel } from "antd";
+import { CarouselRef } from "antd/es/carousel";
+import { useRef } from "react";
 
 export default function SlideShowBanner() {
+  const ref = useRef<CarouselRef>(null);
+  const handlePrevBanner = ()=>{
+    ref.current?.prev()
+  }
+  const handleNextBanner = ()=>{
+    ref.current?.next()
+  }
   return (
     <div className="relative">
-      <Carousel autoplay={true} autoplaySpeed={3000} speed={1500}>
+      <Carousel  infinite={true} ref={ref} autoplay={true} autoplaySpeed={3000} speed={1000}>
         <div>
           <img
             src="https://pos.nvncdn.com/8ca22b-20641/bn/20241018_mdsPcOeq.gif"
@@ -13,16 +22,16 @@ export default function SlideShowBanner() {
         </div>
         <div>
           <img
-            src="https://pos.nvncdn.com/8ca22b-20641/bn/20241018_mdsPcOeq.gif"
+            src="https://pos.nvncdn.com/8ca22b-20641/bn/20241018_syJWHygY.gif"
             alt=""
           />
         </div>
       </Carousel>
-      <div className="absolute top-[50%] left-5 ">
-        <LeftOutlined color="#fff" className="text-white"/>
+      <div onClick={handlePrevBanner} className="absolute top-[50%] left-3 text-white bg-black opacity-65 py-3 px-3 hover:opacity-85 cursor-pointer">
+        <LeftOutlined />
       </div>
-      <div className="absolute top-[50%] right-5">
-        <RightOutlined className="text-white"/>
+      <div onClick={handleNextBanner} className="absolute top-[50%] right-3 text-white bg-black opacity-65 py-3 px-3 hover:opacity-85 cursor-pointer">
+        <RightOutlined />
       </div>
     </div>
   );
