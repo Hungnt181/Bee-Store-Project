e4019ed21f484eb2df6422560cb98ac4d9489f78
dangtien0104 <dangtien0104@gmail.com>
2025-02-15 11:51:49 +0700

    fix: date format

--- fe/src/admin/Color/page.tsx
+++ fe/src/admin/Color/page.tsx
@@ -1,14 +1,14 @@
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import {
   Table,
   Tag,
   message,
   Button,
-  Popconfirm,
   Dropdown,
   TableProps,
   MenuProps,
   Tooltip,
+  notification,
 } from "antd";
 import { MoreOutlined } from "@ant-design/icons"; // Thêm icon Ellipsis
 import axios from "axios";
@@ -19,7 +19,7 @@ import dayjs from "dayjs";
 
 const AdminColorList = () => {
   const navigate = useNavigate(); // Hook điều hướng trang
-  const queryClient = useQueryClient(); // Để làm mới dữ liệu sau khi xóa
+  // Để làm mới dữ liệu sau khi xóa
 
   // Truy vấn dữ liệu từ API
   const {
@@ -36,10 +36,15 @@ const AdminColorList = () => {
           key: color._id, // Đảm bảo bạn có trường _id trong Color
           ...color,
         })); // Đảm bảo trả về mảng
-      } catch (err) {
-        console.error("Lỗi khi lấy dữ liệu:", err);
-        message.error("Lỗi khi lấy danh sách màu!");
-        return []; // Trả về mảng trống khi có lỗi
+      } catch (error: unknown) {
+        if (axios.isAxiosError(error)) {
+          message.error(error.response?.data);
+        } else {
+          notification.error({
+            message: "Chỉnh sửa màu sắc thất bại",
+            placement: "topRight",
+          });
+        }
       }
     },
   });
@@ -62,22 +67,6 @@ const AdminColorList = () => {
   }
 
   // Handle delete color
-  const handleDelete = async (id: string) => {
-    try {
-      const res = await axios.delete(`http://localhost:3000/api/colors/${id}`);
-      if (res.status === 200) {
-        message.success("Xóa màu thành công!");
-        queryClient.invalidateQueries({
-          queryKey: ["colors"],
-        }); // Làm mới dữ liệu sau khi xóa
-      } else {
-        message.error("Không thể xóa màu này.");
-      }
-    } catch (err) {
-      message.error("Lỗi khi xóa màu.");
-      console.error(err);
-    }
-  };
 
   // Cột cho bảng màu
   const columns: TableProps<Color>["columns"] = [
@@ -92,17 +81,25 @@ const AdminColorList = () => {
       title: "Tên màu",
       dataIndex: "name",
       key: "name",
+      render: (value: string) => {
+        return <Tooltip title={value}>{value}</Tooltip>;
+      },
     },
     {
       title: "Mã màu (hexcode)",
       dataIndex: "hexcode",
       key: "hexcode",
-      render: (hexcode: string) => (
-        <Tag color={hexcode} style={{ color: "#fff" }}>
-          {hexcode}
-        </Tag>
-      ),
+      render: (hexcode: string) => {
+        // Kiểm tra nếu mã màu bắt đầu bằng #fffff (theo sau là bất kỳ ký tự nào)
+        const textColor = /^#fffff[a-z0-9]/i.test(hexcode) ? "#000" : "#fff";
+        return (
+          <Tag color={hexcode} style={{ color: textColor, fontWeight: "bold" }}>
+            {hexcode}
+          </Tag>
+        );
+      },
     },
+
     {
       title: "Ngày tạo",
       dataIndex: "createdAt",
@@ -127,7 +124,7 @@ const AdminColorList = () => {
       title: "Thao tác",
       key: "action",
       fixed: "right",
-      align: "right",
+      align: "left",
       render: (_: unknown, color: Color) => {
         const items: MenuProps["items"] = [
           {
@@ -140,24 +137,24 @@ const AdminColorList = () => {
           },
           {
             key: "DELETE",
-            label: (
-              <Popconfirm
-                title="Bạn có chắc chắn muốn xóa màu này?"
-                onConfirm={() => handleDelete(color._id)}
-                okText="Có"
-                cancelText="Không"
-                onCancel={() => message.error("Xóa bị hủy")}
-              >
-                Xóa
-              </Popconfirm>
-            ),
+            label: <a>Xóa</a>,
+            disabled: true,
           },
         ];
         return (
           <>
-            <Button type="primary">Chi tiết</Button>
+            <Button
+              onClick={() => navigate(`/admin/color/detail/${color._id}`)}
+              type="primary"
+            >
+              Chi tiết
+            </Button>
             <Dropdown menu={{ items }}>
-              <MoreOutlined />
+              <Button
+                type="text"
+                icon={<MoreOutlined />}
+                style={{ marginLeft: 8 }}
+              />
             </Dropdown>
           </>
         );
@@ -167,7 +164,7 @@ const AdminColorList = () => {
 
   return (
     <div>
-      <h2>DANH SÁCH MÀU</h2>
+      <h2>DANH SÁCH MÀU SẮC</h2>
       {/* Nút thêm màu */}
       <Button
         type="primary"
--- fe/src/router.tsx
+++ fe/src/router.tsx
@@ -9,6 +9,7 @@ import VariantEditPage from "./admin/Variant/Edit/page";
 import AdminColorList from "./admin/Color/page";
 import AdminColorAdd from "./admin/Color/Add/page";
 import AdminColorEdit from "./admin/Color/Edit/page";
+import AdminColorDetail from "./admin/Color/Detail/page";
 
 export const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
@@ -22,6 +23,7 @@ export const router = createBrowserRouter([
       { path: "/admin/color", element: <AdminColorList /> },
       { path: "/admin/color/add", element: <AdminColorAdd /> },
       { path: "/admin/color/edit/:id", element: <AdminColorEdit /> },
+      { path: "/admin/color/detail/:id", element: <AdminColorDetail /> },
       { path: "/admin/product/:id/edit", element: <ProductEditPage /> },
       { path: "/admin/:id/variant", element: <AdminVariantList /> },
       { path: "/admin/variant/:id/edit", element: <VariantEditPage /> },
