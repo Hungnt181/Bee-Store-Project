1a57b971ac70f143559675c790969d47bdf6b453
Hungnt181 <ngothehung02@gmail.com>
2025-02-17 21:42:25 +0700

    completed search product by name

--- fe/src/admin/Product/page.tsx
+++ fe/src/admin/Product/page.tsx
@@ -1,6 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import {
   Button,
+  Flex,
+  Form,
+  Input,
   Modal,
   Popconfirm,
   Popover,
@@ -8,13 +11,18 @@ import {
   Space,
   Table,
   Tag,
+  Tooltip,
 } from "antd";
 import Product from "../../interface/Product";
 import { Link, useNavigate } from "react-router-dom";
-import { MoreOutlined } from "@ant-design/icons";
+import {
+  MoreOutlined,
+  ReloadOutlined,
+  SearchOutlined,
+} from "@ant-design/icons";
 import useGetAll from "../hooks/useGetAll";
 import useDelete from "../hooks/useDelete";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AdminProductAdd from "./Add/page";
 import dayjs from "dayjs";
 import { Category } from "../../interface/Category";
@@ -24,6 +32,13 @@ const AdminProductList = () => {
   const key = "products";
 
   const { data, isLoading } = useGetAll<Product>(url, key);
+  const [dataTable, setDataTable] = useState<Product[]>([]);
+
+  useEffect(() => {
+    if (data) {
+      setDataTable(data);
+    }
+  }, [data]);
 
   const urlDelete = "http://localhost:3000/api/products/status/";
   const { mutate } = useDelete(urlDelete, key);
@@ -150,9 +165,64 @@ const AdminProductList = () => {
   const handleCancel = () => {
     setIsModalOpen(false);
   };
+  interface SearchValues {
+    searchKey: string;
+  }
+  // Filter
+  const handleFinish = (values: SearchValues) => {
+    if (!values || typeof values.searchKey !== "string") {
+      console.error("Invalid input value");
+      return [];
+    }
+
+    const searchKey = values.searchKey.toLowerCase();
+
+    const newData = data.filter((item: any) => {
+      return (
+        item.name &&
+        typeof item.name === "string" &&
+        item.name.toLowerCase().includes(searchKey)
+      );
+    });
+    setDataTable(newData);
+    return newData;
+  };
+
+  console.log("dataTable", dataTable);
+  const [formSearch] = Form.useForm();
+  const handleRefresh = () => {
+    setDataTable(data);
+    formSearch.resetFields();
+  };
   return (
     <div>
-      <h1>DANH MỤC SẢN PHẨM</h1>
+      <h1 style={{ margin: "0 0 5px 0" }}>DANH MỤC SẢN PHẨM</h1>
+
+      <Flex gap={0} style={{ marginBottom: "30px" }} justify="space-between">
+        <Form
+          form={formSearch}
+          name="searchForm"
+          layout="inline"
+          onFinish={handleFinish}
+        >
+          <Tooltip title="Nhập tên danh mục" placement="right">
+            <Form.Item label={null} name="searchKey" style={{ width: "400px" }}>
+              <Input placeholder="Tìm kiếm z..." prefix={<SearchOutlined />} />
+            </Form.Item>
+          </Tooltip>
+          <Form.Item label={null}>
+            <Button type="primary" htmlType="submit">
+              Tìm kiếm
+            </Button>
+          </Form.Item>
+          <Tooltip title="Làm mới" placement="rightBottom">
+            <ReloadOutlined
+              style={{ cursor: "pointer" }}
+              onClick={handleRefresh}
+            />
+          </Tooltip>
+        </Form>
+      </Flex>
       <Skeleton loading={isLoading}>
         <Button
           type="primary"
@@ -163,7 +233,7 @@ const AdminProductList = () => {
         </Button>
 
         <Table
-          dataSource={data}
+          dataSource={dataTable.length !== null ? dataTable : data}
           columns={columns}
           pagination={{ pageSize: 10 }}
           scroll={{ y: 55 * 8 }}
