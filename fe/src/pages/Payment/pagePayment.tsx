317f3feed7f5ecb3ec64ea09f1abf433f636029a
hoan <hoan123nh@gmail.com>
2025-03-13 18:01:20 +0700

    truyen data

--- fe/src/pages/Payment/pagePayment.tsx
+++ fe/src/pages/Payment/pagePayment.tsx
@@ -1,74 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { User, Phone, Mail, MapPin, Pencil, Tag, Lock } from "lucide-react";
+import axios from "axios";
 
 interface Districts {
   [key: string]: string[];
 }
+
+interface CartItemDetail {
+  idProduct: string,
+  idVariant: string,
+  color: string,
+  size: string,
+  quantity: number,
+}
+interface ItemCheckout {
+  nameProduct: string,
+  imgVariant: string,
+  price: number,
+  color: string,
+  size: string,
+  quantity: number,
+  totalPrice: number,
+}
+
+
 //Tỉnh và Thành phố
 const provinces = [
   "Hà Nội",
   "Hồ Chí Minh",
-  "Đà Nẵng",
-  "Hải Phòng",
-  "Cần Thơ",
-  "An Giang",
-  "Bà Rịa - Vũng Tàu",
-  "Bắc Giang",
-  "Bắc Kạn",
-  "Bạc Liêu",
-  "Bắc Ninh",
-  "Bến Tre",
-  "Bình Định",
-  "Bình Dương",
-  "Bình Phước",
-  "Bình Thuận",
-  "Cà Mau",
-  "Cao Bằng",
-  "Đắk Lắk",
-  "Đắk Nông",
-  "Điện Biên",
-  "Đồng Nai",
-  "Đồng Tháp",
-  "Gia Lai",
-  "Hà Giang",
-  "Hà Nam",
-  "Hà Tĩnh",
-  "Hải Dương",
-  "Hậu Giang",
-  "Hòa Bình",
-  "Hưng Yên",
-  "Khánh Hòa",
-  "Kiên Giang",
-  "Kon Tum",
-  "Lai Châu",
-  "Lâm Đồng",
-  "Lạng Sơn",
-  "Lào Cai",
-  "Long An",
-  "Nam Định",
-  "Nghệ An",
-  "Ninh Bình",
-  "Ninh Thuận",
-  "Phú Thọ",
-  "Phú Yên",
-  "Quảng Bình",
-  "Quảng Nam",
-  "Quảng Ngãi",
-  "Quảng Ninh",
-  "Quảng Trị",
-  "Sóc Trăng",
-  "Sơn La",
-  "Tây Ninh",
-  "Thái Bình",
-  "Thái Nguyên",
-  "Thanh Hóa",
-  "Thừa Thiên Huế",
-  "Tiền Giang",
-  "Trà Vinh",
-  "Tuyên Quang",
-  "Vĩnh Long",
-  "Vĩnh Phúc",
-  "Yên Bái",
 ];
 //Quận và Huyện
 const districts: Districts = {
@@ -127,377 +86,8 @@ const districts: Districts = {
     "Hóc Môn",
     "Nhà Bè",
   ],
-  "Đà Nẵng": [
-    "Hải Châu",
-    "Thanh Khê",
-    "Sơn Trà",
-    "Ngũ Hành Sơn",
-    "Liên Chiểu",
-    "Cẩm Lệ",
-    "Hòa Vang",
-    "Hoàng Sa",
-  ],
-  "Hải Phòng": [
-    "Hồng Bàng",
-    "Ngô Quyền",
-    "Lê Chân",
-    "Hải An",
-    "Kiến An",
-    "Đồ Sơn",
-    "Dương Kinh",
-    "Thủy Nguyên",
-    "An Dương",
-    "An Lão",
-    "Kiến Thụy",
-    "Tiên Lãng",
-    "Vĩnh Bảo",
-    "Cát Hải",
-    "Bạch Long Vĩ",
-  ],
-  "Cần Thơ": [
-    "Ninh Kiều",
-    "Bình Thủy",
-    "Cái Răng",
-    "Ô Môn",
-    "Thốt Nốt",
-    "Phong Điền",
-    "Cờ Đỏ",
-    "Thới Lai",
-    "Vĩnh Thạnh",
-  ],
-  "An Giang": [
-    "Long Xuyên",
-    "Châu Đốc",
-    "Tân Châu",
-    "An Phú",
-    "Châu Phú",
-    "Châu Thành",
-    "Phú Tân",
-    "Thoại Sơn",
-    "Tri Tôn",
-    "Tịnh Biên",
-  ],
-  "Bà Rịa - Vũng Tàu": [
-    "Vũng Tàu",
-    "Bà Rịa",
-    "Châu Đức",
-    "Côn Đảo",
-    "Đất Đỏ",
-    "Long Điền",
-    "Tân Thành",
-    "Xuyên Mộc",
-  ],
-  "Hà Giang": [
-    "Hà Giang",
-    "Bắc Mê",
-    "Bắc Quang",
-    "Đồng Văn",
-    "Hoàng Su Phì",
-    "Mèo Vạc",
-    "Quản Bạ",
-    "Quang Bình",
-    "Vị Xuyên",
-    "Xín Mần",
-    "Yên Minh",
-  ],
-  "Hà Nam": [
-    "Phủ Lý",
-    "Bình Lục",
-    "Duy Tiên",
-    "Kim Bảng",
-    "Lý Nhân",
-    "Thanh Liêm",
-  ],
-  "Hà Tĩnh": [
-    "Hà Tĩnh",
-    "Hồng Lĩnh",
-    "Kỳ Anh",
-    "Cẩm Xuyên",
-    "Can Lộc",
-    "Đức Thọ",
-    "Hương Khê",
-    "Hương Sơn",
-    "Lộc Hà",
-    "Nghi Xuân",
-    "Thạch Hà",
-    "Vũ Quang",
-  ],
-  "Hải Dương": [
-    "Hải Dương",
-    "Chí Linh",
-    "Cẩm Giàng",
-    "Gia Lộc",
-    "Kim Thành",
-    "Kinh Môn",
-    "Nam Sách",
-    "Ninh Giang",
-    "Thanh Hà",
-    "Thanh Miện",
-    "Tứ Kỳ",
-    "Bình Giang",
-  ],
-  "Hậu Giang": [
-    "Vị Thanh",
-    "Ngã Bảy",
-    "Châu Thành",
-    "Châu Thành A",
-    "Long Mỹ",
-    "Phụng Hiệp",
-    "Vị Thủy",
-  ],
-  "Hòa Bình": [
-    "Hòa Bình",
-    "Cao Phong",
-    "Đà Bắc",
-    "Kim Bôi",
-    "Kỳ Sơn",
-    "Lạc Sơn",
-    "Lạc Thủy",
-    "Lương Sơn",
-    "Mai Châu",
-    "Tân Lạc",
-    "Yên Thủy",
-  ],
-  "Hưng Yên": [
-    "Hưng Yên",
-    "Ân Thi",
-    "Khoái Châu",
-    "Kim Động",
-    "Mỹ Hào",
-    "Phù Cừ",
-    "Tiên Lữ",
-    "Văn Giang",
-    "Văn Lâm",
-    "Yên Mỹ",
-  ],
-  "Khánh Hòa": [
-    "Nha Trang",
-    "Cam Ranh",
-    "Ninh Hòa",
-    "Vạn Ninh",
-    "Diên Khánh",
-    "Khánh Sơn",
-    "Khánh Vĩnh",
-    "Trường Sa",
-  ],
-  "Kiên Giang": [
-    "Rạch Giá",
-    "Hà Tiên",
-    "An Biên",
-    "An Minh",
-    "Châu Thành",
-    "Giang Thành",
-    "Giồng Riềng",
-    "Gò Quao",
-    "Hòn Đất",
-    "Kiên Hải",
-    "Kiên Lương",
-    "Tân Hiệp",
-    "U Minh Thượng",
-    "Vĩnh Thuận",
-    "Phú Quốc",
-  ],
-  "Kon Tum": [
-    "Kon Tum",
-    "Đắk Glei",
-    "Đắk Hà",
-    "Đắk Tô",
-    "Ia H’Drai",
-    "Kon Plông",
-    "Kon Rẫy",
-    "Ngọc Hồi",
-    "Sa Thầy",
-    "Tu Mơ Rông",
-  ],
-  "Lai Châu": [
-    "Lai Châu",
-    "Mường Tè",
-    "Nậm Nhùn",
-    "Phong Thổ",
-    "Sìn Hồ",
-    "Tam Đường",
-    "Tân Uyên",
-    "Than Uyên",
-  ],
-  "Lâm Đồng": [
-    "Đà Lạt",
-    "Bảo Lộc",
-    "Bảo Lâm",
-    "Cát Tiên",
-    "Đạ Huoai",
-    "Đạ Tẻh",
-    "Đam Rông",
-    "Di Linh",
-    "Đơn Dương",
-    "Đức Trọng",
-    "Lạc Dương",
-    "Lâm Hà",
-  ],
-  "Lạng Sơn": [
-    "Lạng Sơn",
-    "Bắc Sơn",
-    "Bình Gia",
-    "Cao Lộc",
-    "Chi Lăng",
-    "Đình Lập",
-    "Hữu Lũng",
-    "Lộc Bình",
-    "Tràng Định",
-    "Văn Lãng",
-    "Văn Quan",
-  ],
-  "Lào Cai": [
-    "Lào Cai",
-    "Bát Xát",
-    "Bảo Thắng",
-    "Bảo Yên",
-    "Bắc Hà",
-    "Mường Khương",
-    "Sa Pa",
-    "Si Ma Cai",
-    "Văn Bàn",
-  ],
-  "Long An": [
-    "Tân An",
-    "Bến Lức",
-    "Cần Đước",
-    "Cần Giuộc",
-    "Châu Thành",
-    "Đức Hòa",
-    "Đức Huệ",
-    "Mộc Hóa",
-    "Tân Hưng",
-    "Tân Thạnh",
-    "Tân Trụ",
-    "Thạnh Hóa",
-    "Thủ Thừa",
-    "Vĩnh Hưng",
-  ],
-  "Nam Định": [
-    "Nam Định",
-    "Giao Thủy",
-    "Hải Hậu",
-    "Mỹ Lộc",
-    "Nam Trực",
-    "Nghĩa Hưng",
-    "Trực Ninh",
-    "Vụ Bản",
-    "Xuân Trường",
-    "Ý Yên",
-  ],
-  "Nghệ An": [
-    "Vinh",
-    "Cửa Lò",
-    "Hoàng Mai",
-    "Thái Hòa",
-    "Anh Sơn",
-    "Con Cuông",
-    "Diễn Châu",
-    "Đô Lương",
-    "Hưng Nguyên",
-    "Kỳ Sơn",
-    "Nam Đàn",
-    "Nghi Lộc",
-    "Nghĩa Đàn",
-    "Quế Phong",
-    "Quỳ Châu",
-    "Quỳ Hợp",
-    "Quỳnh Lưu",
-    "Tân Kỳ",
-    "Thanh Chương",
-    "Tương Dương",
-    "Yên Thành",
-  ],
-  "Ninh Bình": [
-    "Ninh Bình",
-    "Tam Điệp",
-    "Gia Viễn",
-    "Hoa Lư",
-    "Kim Sơn",
-    "Nho Quan",
-    "Yên Khánh",
-    "Yên Mô",
-  ],
-  "Ninh Thuận": [
-    "Phan Rang - Tháp Chàm",
-    "Bác Ái",
-    "Ninh Hải",
-    "Ninh Phước",
-    "Ninh Sơn",
-    "Thuận Bắc",
-    "Thuận Nam",
-  ],
-  "Phú Thọ": [
-    "Việt Trì",
-    "Phú Thọ",
-    "Cẩm Khê",
-    "Đoan Hùng",
-    "Hạ Hòa",
-    "Lâm Thao",
-    "Tam Nông",
-    "Tân Sơn",
-    "Thanh Ba",
-    "Thanh Sơn",
-    "Thanh Thủy",
-    "Yên Lập",
-  ],
-  "Phú Yên": [
-    "Tuy Hòa",
-    "Sông Cầu",
-    "Đồng Xuân",
-    "Sơn Hòa",
-    "Sông Hinh",
-    "Tây Hòa",
-    "Tuy An",
-    "Phú Hòa",
-    "Đông Hòa",
-  ],
-  "Quảng Bình": [
-    "Đồng Hới",
-    "Ba Đồn",
-    "Bố Trạch",
-    "Lệ Thủy",
-    "Minh Hóa",
-    "Quảng Ninh",
-    "Quảng Trạch",
-    "Tuyên Hóa",
-  ],
-  "Quảng Nam": [
-    "Tam Kỳ",
-    "Hội An",
-    "Bắc Trà My",
-    "Đại Lộc",
-    "Điện Bàn",
-    "Duy Xuyên",
-    "Hiệp Đức",
-    "Nam Giang",
-    "Nam Trà My",
-    "Nông Sơn",
-    "Núi Thành",
-    "Phú Ninh",
-    "Phước Sơn",
-    "Quế Sơn",
-    "Tây Giang",
-    "Thăng Bình",
-    "Tiên Phước",
-  ],
-  "Quảng Ngãi": [
-    "Quảng Ngãi",
-    "Ba Tơ",
-    "Bình Sơn",
-    "Đức Phổ",
-    "Lý Sơn",
-    "Minh Long",
-    "Mộ Đức",
-    "Nghĩa Hành",
-    "Sơn Hà",
-    "Sơn Tây",
-    "Sơn Tịnh",
-    "Tây Trà",
-    "Trà Bồng",
-    "Tư Nghĩa",
-  ],
 };
+
 
 const PaymentPage = () => {
   const [quantity] = useState(1);
@@ -505,6 +95,10 @@ const PaymentPage = () => {
   const [selectedDistrict, setSelectedDistrict] = useState("");
   const [selectedPayment, setSelectedPayment] = useState<string>("cod"); // Mặc định chọn "cod"
 
+  const [cartItems, setCartItems] = useState<CartItemDetail[]>([]);
+  const [listCheckout, setListCheckout] = useState<ItemCheckout[]>([]);
+  const [totalPrice, setTotalPrice] = useState<number>(0);
+  // dữ liệu mẫu
   const orders = [
     {
       id: 1,
@@ -516,6 +110,74 @@ const PaymentPage = () => {
     },
   ];
 
+  // lấy cartItems từ localstorage
+  useEffect(() => {
+    const storedCartItems = localStorage.getItem('cartItems');
+    if (storedCartItems) {
+      setCartItems(JSON.parse(storedCartItems));
+    }
+  }, []);
+  //lấy sp tu id
+  const getPdts = async (idProduct: string) => {
+    try {
+      const { data } = await axios.get('http://localhost:3000/api/products/' + idProduct);
+      // console.log(data.data);
+      return data.data
+    }
+    catch (error) {
+      console.log("ko lấy đc sp từ id");
+    }
+  }
+  // lấy variant tu id
+  const getVariant = async (idVariant: string) => {
+    try {
+      const { data } = await axios.get('http://localhost:3000/api/variants/' + idVariant);
+      console.log(data.variants[0]);
+      return data.variants[0]
+    }
+    catch (error) {
+      console.log("ko lấy đc bien the từ id");
+    }
+  }
+
+  // tao item cho listCheckout
+  const fetchProducts = async () => {
+    const updatedListCheckout: ItemCheckout[] = [];
+    for (const cartItem of cartItems) {
+      const productData = await getPdts(cartItem.idProduct);
+      const variantData = await getVariant(cartItem.idVariant);
+      console.log('var' + variantData.image[0]);
+
+
+      if (productData && variantData) {
+        const newItemCheckout: ItemCheckout = {
+          nameProduct: productData.name,
+          imgVariant: variantData.image[0],
+          price: productData.price,
+          color: cartItem.color,
+          size: cartItem.size,
+          quantity: cartItem.quantity,
+          totalPrice: productData.price * cartItem.quantity,
+        };
+        updatedListCheckout.push(newItemCheckout);
+      }
+    };
+    setListCheckout(updatedListCheckout);
+    calculateTotalPrice(updatedListCheckout)
+    console.log(listCheckout);
+    
+  };
+  useEffect(() => {
+    fetchProducts();
+  }, [cartItems]);
+  // console.log(listCheckout);
+
+  // tinh tong gia tien
+  const calculateTotalPrice = (items: ItemCheckout[]) => {
+    const total = items.reduce((sum, item) => sum + item.totalPrice, 0);
+    setTotalPrice(total);
+  };
+
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100 p-4">
       <div className="bg-white p-6 rounded-lg shadow-xl w-full max-w-7xl">
@@ -524,31 +186,31 @@ const PaymentPage = () => {
           Thông tin sản phẩm
         </h2>
         <div className="border border-gray-300 p-4 mb-6 rounded-lg bg-gray-50">
-          {orders.map((order) => (
+          {listCheckout.map((item:ItemCheckout, index:number) => (
             <div
-              key={order.id}
+              key={index}
               className="flex gap-4 mb-4 border-b border-gray-300 pb-4 last:border-b-0"
             >
               <img
-                src={order.image}
-                alt={order.name}
+                src={item.imgVariant}
+                alt={item.nameProduct}
                 className="w-24 h-24 object-cover rounded-lg"
               />
               <div className="flex-1">
-                <p className="font-semibold">{order.name}</p>
+                <p className="font-semibold">{item.nameProduct}</p>
                 <p className="text-sm text-gray-600">
-                  Mã sản phẩm: {order.code}
+                  Mã sản phẩm: {item.color}
                 </p>
-                <p className="line-through text-gray-500">
-                  {order.oldPrice.toLocaleString()} đ
-                </p>
+                {/* <p className="line-through text-gray-500">
+                  {item.price} đ
+                </p> */}
                 <div className="flex justify-between items-center mt-1">
                   <p className="text-lg font-semibold text-black-500">
-                    {order.newPrice.toLocaleString()} đ
+                    {item.price} đ
                   </p>
                   <div className="flex items-center gap-2">
                     <span className="w-12  p-1 rounded-md text-center">
-                      x {quantity}
+                      x {item.quantity}
                     </span>
                   </div>
                 </div>
@@ -686,7 +348,7 @@ const PaymentPage = () => {
           <div className="flex-1 border border-gray-300 p-6 bg-gray-100 rounded-lg">
             <h3 className="text-lg font-semibold mb-2">Thanh toán</h3>
             <p className="flex justify-between text-gray-500">
-              Tổng giá trị sản phẩm <span>1,900,000 đ</span>
+              Tổng giá trị sản phẩm <span>{totalPrice} đ</span>
             </p>
             <p className="flex justify-between text-gray-500">
               Giảm giá <span>0 đ</span>
